ee9d799ab061ebe63a5d576342c2600060fb7750
rzhangsan <4iNtJYlBfhr1QT8bkeMawE+rzhangsan@noreply.cnb.cool>
2025-09-10 19:09:32 +0800

    hhh

--- src/audio.rs
+++ src/audio.rs
@@ -31,9 +31,9 @@ unsafe fn afe_init() -> (
     afe_config.pcm_config.ref_num = 0;
     afe_config.pcm_config.sample_rate = SAMPLE_RATE as i32;
     afe_config.afe_ringbuf_size = 64;
-    // afe_config.vad_min_noise_ms = 500;
-    // afe_config.vad_mode = esp_sr::vad_mode_t_VAD_MODE_1;
-    // afe_config.agc_init = true;
+    afe_config.vad_min_noise_ms = 500;
+    afe_config.vad_mode = esp_sr::vad_mode_t_VAD_MODE_1;
+    afe_config.agc_init = true;
     afe_config.memory_alloc_mode = esp_sr::afe_memory_alloc_mode_t_AFE_MEMORY_ALLOC_MORE_PSRAM;
     afe_config.debug_init = true;
 
