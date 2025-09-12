798a0398098c95282a5e10606a61eab4114038c4
rzhangsan <4iNtJYlBfhr1QT8bkeMawE+rzhangsan@noreply.cnb.cool>
2025-09-12 20:02:03 +0800

    hhh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cargo build --release
-# find srmodels.bin
-srmodels_bin_path=`find . -name srmodels.bin | grep release`
-echo $srmodels_bin_path
-# copy srmodels.bin
-cp -f $srmodels_bin_path .
-cp -f ./target/xtensa-esp32s3-espidf/release/echokit .
+# # find srmodels.bin
+# srmodels_bin_path=`find . -name srmodels.bin | grep release`
+# echo $srmodels_bin_path
+# # copy srmodels.bin
+# cp -f $srmodels_bin_path .
+# cp -f ./target/xtensa-esp32s3-espidf/release/echokit .
 
 ls -lah
